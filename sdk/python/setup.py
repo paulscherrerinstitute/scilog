0a53e0fb2d6b76b3fd0e46d37d91bee38efcaf82
minottic <carlo.minotti@psi.ch>
2022-08-04 08:22:32 +0200

    Add setup.py to pip install

---
+++ sdk/python/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='scilog',
+    version='1.0',
+    description='Scilog SDK',
+    author='Klaus Wakonig',
+    author_email='klaus.wakonig@psi.ch',
+    packages=[
+      'scilog',
+      ],
+    install_requires=[
+      "requests >= 2.28.1",
+      ]
+    )
