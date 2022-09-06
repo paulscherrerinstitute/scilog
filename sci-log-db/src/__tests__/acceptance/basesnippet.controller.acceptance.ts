84345a8ab7a4401768c5376fdbc9179fe05f017b
egli <stephan.egli@psi.ch>
2022-09-06 15:12:30 +0200

    Further work on test cases

--- sci-log-db/src/__tests__/acceptance/basesnippet.controller.acceptance.ts
+++ sci-log-db/src/__tests__/acceptance/basesnippet.controller.acceptance.ts
@@ -24,7 +24,7 @@ describe('Base snippet controller services', function (this: Suite) {
   before(clearDatabase);
   before('login', async () => {
     user = await createAUser(app)
-    const loginResponse = await client.post('/users/login').send({ principal: user.email, password: userPassword })
+    const loginResponse = await client.post('/users/login').send({ principal: user.email, password: userPassword})
     token = loginResponse.body.token
   })
   after(clearDatabase);
@@ -33,26 +33,54 @@ describe('Base snippet controller services', function (this: Suite) {
     if (app != null) await app.stop();
   });
 
-  it('tries to create location', async () => {
-    let acls={
-      createACL:['unx_sf-bernina-bs'],
-      readACL:['any-authenticated-user'],
-      updateACL:['unx_sf-bernina-bs'],
-      deleteACL:['unx_sf-bernina-bs'],
-      shareACL:[''],
-      adminACL:['unx_sf-bernina-bs'],
+  it('tries to create location and linked logbook', async () => {
+    let acls = {
+      createACL: ['unx_sf-bernina-bs'],
+      readACL: ['p12345','unx_sf-bernina-bs'],
+      updateACL: ['p12345','unx_sf-bernina-bs'],
+      deleteACL: ['unx_sf-bernina-bs'],
+      shareACL: [user.email,'p12345'],
+      adminACL: [user.email,'unx_sf-bernina-bs'],
     }
-    await client.post('/locations')
+    let logbook = {
+      snippetType: 'logbook',
+      isPrivate: false,
+      name: 'My logbook',
+      description: 'Logbook description goes here',
+       ...acls
+    }
+
+    const postResponse = await client.post('/locations')
       .set('Authorization', 'Bearer ' + token)
-      .send({snippetType: 'location', textcontent:'Abstract goes here...', ...acls})
+      .send({ snippetType: 'location', textcontent: 'Abstract goes here...', ...acls })
       .expect(200)
-      .then((result) =>
-        expect(result.body.snippetType).to.be.eql("location"),
+      .then()
+      .catch((err) => {
+        throw err;
+      });
+    var logbookId;
+    await client.post('/logbooks')
+      .set('Authorization', 'Bearer ' + token)
+      .send({ ...logbook, location: `${postResponse.body.id}` })
+      .expect(200)
+      .then((result) => {
+        logbookId = result.body.id
+        // console.log("Found logbook id:",logbookId)
+        expect(result.body.snippetType).to.be.eql("logbook")
+        }
       )
       .catch((err) => {
         throw err;
       });
+
+    const logbookResponse=await client.get(`/basesnippets/${logbookId}`)
+    .set('Authorization', 'Bearer ' + token)
+    .expect(200)
+    .then( )
+    .catch((err) => {
+      throw err;
+    });
+
+    // console.log("Logbook response:", logbookResponse.body)
   });
-
-
 })
