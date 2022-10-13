ec3bd437662b7851ffa48dcdcad08f3adacee6d3
Carlo Minotti <minotti_c@Carlos-MBP.yallo.box>
2022-10-13 16:07:09 +0200

    Reuse mixin for task controller

--- sci-log-db/src/repositories/task.repository.ts
+++ sci-log-db/src/repositories/task.repository.ts
@@ -1,13 +1,18 @@
-import {inject} from '@loopback/core';
+import {Constructor, inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
 import {Task, TaskRelations} from '../models';
 import {AutoAddRepository} from './autoadd.repository.base';
+import {SnippetRepositoryMixin} from '../mixins/basesnippet.repository-mixin';
 
-export class TaskRepository extends AutoAddRepository<
+export class TaskRepository extends SnippetRepositoryMixin<
   Task,
   typeof Task.prototype.id,
-  TaskRelations
-> {
+  TaskRelations,
+  Constructor<
+    DefaultCrudRepository<Task, typeof Task.prototype.id, TaskRelations>
+  >
+>(AutoAddRepository) {
   constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
     super(Task, dataSource);
   }
