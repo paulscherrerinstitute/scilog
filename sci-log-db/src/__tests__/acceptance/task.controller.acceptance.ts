a5704bd3776dd5edc8d27ff532fd91f35fcadd82
Carlo Minotti <minotti_c@Carlos-MBP.yallo.box>
2022-10-13 16:35:54 +0200

    Add tasks tests for the code change

---
+++ sci-log-db/src/__tests__/acceptance/task.controller.acceptance.ts
@@ -0,0 +1,271 @@
+import {Client, expect} from '@loopback/testlab';
+import _ from 'lodash';
+import {Suite} from 'mocha';
+import {SciLogDbApplication} from '../..';
+import {clearDatabase, createUserToken, setupApplication} from './test-helper';
+
+describe('TaskRepositorySnippet', function (this: Suite) {
+  this.timeout(5000);
+  let app: SciLogDbApplication;
+  let client: Client;
+  let token: string;
+  let taskSnippetId: string;
+  const taskSnippet = {
+    ownerGroup: 'aOwner',
+    accessGroups: ['taskAcceptance'],
+    isPrivate: true,
+    defaultOrder: 0,
+    expiresAt: '2055-10-10T14:04:19.522Z',
+    tags: ['aSearchableTag'],
+    dashboardName: 'string',
+    versionable: true,
+  };
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+    await clearDatabase(app);
+    token = await createUserToken(app, client, ['taskAcceptance']);
+  });
+
+  after(async () => {
+    await clearDatabase(app);
+    if (app != null) await app.stop();
+  });
+
+  it('post a task without token should return 401', async () => {
+    await client.post('/tasks').send(taskSnippet).expect(401);
+  });
+
+  it('post a task with authentication should return 200 and contain taskSnippet and be of type task', async () => {
+    await client
+      .post('/tasks')
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .send(taskSnippet)
+      .expect(200)
+      .then(
+        result => (
+          expect(result.body).to.containEql(taskSnippet),
+          expect(result.body.snippetType).to.containEql('task'),
+          (taskSnippetId = result.body.id)
+        ),
+      )
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('count snippet without token should return 401', async () => {
+    await client
+      .get('/tasks/count')
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('count snippet with token should return body.count=1', async () => {
+    await client
+      .get('/tasks/count')
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .then(result => expect(result.body.count).to.eql(1))
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('get snippets without token should return 401', async () => {
+    await client
+      .get('/tasks')
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('get snippet with token should be of length one and contain task', async () => {
+    await client
+      .get('/tasks')
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .then(
+        result => (
+          expect(result.body.length).to.be.eql(1),
+          expect(result.body[0]).to.containEql(taskSnippet)
+        ),
+      )
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('get snippets with ID without token should return 401', async () => {
+    await client
+      .get(`/tasks/${taskSnippetId}`)
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('get snippet with ID with token should return 200 and contain task', async () => {
+    await client
+      .get(`/tasks/${taskSnippetId}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .then(result => expect(result.body).to.containEql(taskSnippet))
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('patch snippet without token should return 401', async () => {
+    await client
+      .patch('/tasks/')
+      .set('Content-Type', 'application/json')
+      .send({dashboardName: 'aNewName'})
+      .expect(401);
+  });
+
+  it('patch snippet with toke should return 200 and body.count=1', async () => {
+    await client
+      .patch('/tasks')
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .send({dashboardName: 'aNewName'})
+      .expect(200)
+      .then(result => expect(result.body.count).to.be.eql(1))
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('put snippet without token should return 401', async () => {
+    await client
+      .put(`/task/${taskSnippetId}`)
+      .send(taskSnippet)
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('put snippet with token should return 204', async () => {
+    const taskSnippetPut = {
+      ..._.omit(taskSnippet, 'dashboardName'),
+      dashboardName: 'dashboardNamePut',
+    };
+    await client
+      .put(`/task/${taskSnippetId}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .send(taskSnippetPut)
+      .expect(204);
+  });
+
+  it('Get index without token should return 401', async () => {
+    await client
+      .get(`/tasks/index=${taskSnippetId}`)
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('Get index with token should return 200 and body=0', async () => {
+    await client
+      .get(`/tasks/index=${taskSnippetId}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .then(result => expect(result.body).to.be.eql(0))
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('Search index with token should return 200 and matching body.tags', async () => {
+    const includeTags = {fields: {tags: true}, include: ['subsnippets']};
+    await client
+      .get(`/tasks/search=aSearchabletag?filter=${JSON.stringify(includeTags)}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .then(
+        result => (
+          expect(result.body.length).to.be.eql(1),
+          expect(result.body[0].tags[0]).to.be.eql('aSearchableTag')
+        ),
+      )
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('patch snippet by id without token should return 401', async () => {
+    await client
+      .patch(`/tasks/${taskSnippetId}`)
+      .send(taskSnippet)
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  // when patching by id, to preserve the history, the original snippet is duplicated, then a history snippet is created and then the original one is updated
+  // to keep the linking between the three, the parentId of the duplicated one = id of the history snippet and the parentId of the history snippet = id of the original one
+  // to prevent the search to return too many snippets, the fields used in the subsequent search are updated
+  it('patch snippet by id with token should return 204', async () => {
+    await client
+      .patch(`/tasks/${taskSnippetId}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .send({
+        tags: ['aSearchExcludedTag'],
+        name: 'aSearchExcludedName',
+        description: 'aSearchExcludedDescription',
+        textcontent: 'aSearchExcludedTextContent',
+      })
+      .expect(204);
+  });
+
+  it('Search index with token should return 200 and matching subsnippets', async () => {
+    const includeTags = {fields: {tags: true}, include: ['subsnippets']};
+    await client
+      .get(`/tasks/search=aSearchabletag?filter=${JSON.stringify(includeTags)}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .then(
+        result => (
+          expect(result.body.length).to.be.eql(1),
+          expect(result.body[0].snippetType).to.be.eql('task'),
+          expect(result.body[0].tags).to.be.eql(['aSearchableTag'])
+        ),
+      )
+      .catch(err => {
+        throw err;
+      });
+  });
+
+  it('delete task by id without token should return 401', async () => {
+    await client
+      .delete(`/tasks/${taskSnippetId}`)
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('delete task by id with token should return 204', async () => {
+    await client
+      .delete(`/tasks/${taskSnippetId}`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(204);
+  });
+  it('restore snippet by id without token should return 401', async () => {
+    await client
+      .patch(`/tasks/${taskSnippetId}/restore`)
+      .set('Content-Type', 'application/json')
+      .expect(401);
+  });
+
+  it('restore snippet by id with token should return 204', async () => {
+    await client
+      .patch(`/tasks/${taskSnippetId}/restore`)
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json')
+      .expect(204);
+  });
+});
