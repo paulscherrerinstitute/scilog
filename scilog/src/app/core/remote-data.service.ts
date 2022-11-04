3e621ba80ce1aac9eec4924507b3508324c12c6e
Carlo Minotti <minotti_c@Carlos-MBP.yallo.box>
2022-11-04 23:35:51 +0100

    Revert changes

--- scilog/src/app/core/remote-data.service.ts
+++ scilog/src/app/core/remote-data.service.ts
@@ -97,7 +97,7 @@ export class LogbookItemDataService extends RemoteDataService {
     }
 
     let whereFilter: Object[] = [];
-    whereFilter.push({ "or": [{ "snippetType": "paragraph" }, { "snippetType": "image" }, { "snippetType": "filesnippet" }] });
+    whereFilter.push({ "or": [{ "snippetType": "paragraph" }, { "snippetType": "image" }] });
 
     let parentIds: string[] = [];
     if ((config.filter?.targetId) && (config.filter.targetId.length > 1)) {
