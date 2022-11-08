a28530326cca1be6a04272df017f5d891d37e9af
Carlo Minotti <minotti_c@Carlos-MBP.yallo.box>
2022-11-08 15:44:38 +0100

    Reload data on updates

--- scilog/src/app/overview/overview.component.ts
+++ scilog/src/app/overview/overview.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Logbooks } from '@model/logbooks';
 import { Subject, Subscription } from 'rxjs';
 import { UserPreferencesService } from '@shared/user-preferences.service';
 import { CollectionConfig, WidgetItemConfig } from '@model/config';
-import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AddCollectionComponent } from './add-collection/add-collection.component';
 import { AddLogbookComponent } from './add-logbook/add-logbook.component';
 import { LogbookInfoService } from '@shared/logbook-info.service';
@@ -31,7 +31,6 @@ export class OverviewComponent implements OnInit {
 
   showViewSelection = false;
   collections: CollectionConfig[];
-  logbooks: Logbooks[];
   viewMode: string;
 
   fileToUpload: File = null;
@@ -68,31 +67,12 @@ export class OverviewComponent implements OnInit {
       }
       this.config = this._prepareConfig();
       this.logbookIconScrollService.initialize(this.config);
-      this.getLogbookData();
     }));
     this.subscriptions.push(this.searchStringSubject.pipe(debounceTime(500)).subscribe(() => {
       this.logbookIconScrollService.config = this._prepareConfig();
       this.logbookIconScrollService.reset();
 
     }));
-  }
-
-  async getLogbookData() {
-    await this.logbookInfo.getAvailLogbooks();
-    this.logbooks = this.logbookInfo.availLogbooks.slice(0, 500);
-    console.log(this.logbooks);
-    if (sessionStorage.getItem('scilog-auto-selection-logbook') == null) {
-      sessionStorage.setItem('scilog-auto-selection-logbook', 'true');
-      if (this.cookie.lastLogbook != "") {
-        this.logbooks.forEach(logbook => {
-          if (logbook.id == this.cookie.lastLogbook) {
-            this.logbookInfo.logbookInfo = logbook;
-            this.router.navigateByUrl('/logbooks/' + this.cookie.lastLogbook + '/dashboard');
-            console.log("last logbook: ", this.cookie.lastLogbook)
-          }
-        })
-      }
-    }
   }
 
   collectionSelected(collection: CollectionConfig) {
@@ -125,51 +105,22 @@ export class OverviewComponent implements OnInit {
     this.fileToUpload = files.item(0);
   }
 
-  filterItem(searchTag: string) {
-    console.log(this.logbooks.filter((item: Logbooks) => {
-      for (const key in item) {
-        if (Object.prototype.hasOwnProperty.call(item, key)) {
-          const element = item[key];
-          if (typeof element == "string") {
-            return element.toLowerCase().includes(searchTag.toLowerCase());
-          }
-        }
-      }
-      return true;
-
-    }));
-    console.log(this.logbooks);
-  }
-
   editLogbook(logbook: Logbooks) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     dialogConfig.data = logbook;
     let dialogRef: any;
-    let logIndex: number = null;
     dialogRef = this.dialog.open(AddLogbookComponent, dialogConfig);
 
-    this.subscriptions.push(dialogRef.afterClosed().subscribe(result => {
+    this.subscriptions.push(dialogRef.afterClosed().subscribe(async result => {
       console.log("Dialog result:", result);
-      logIndex = this.logbooks.findIndex(logbook => {
-        return logbook.id == result.id;
-      })
-      if (logIndex != null) {
-        this.logbooks[logIndex] = result;
-      }
+      await this.logbookIconScrollService.reload();
     }));
   }
 
   async deleteLogbook(logbookId: string) {
-    let logIndex: number = null;
-    logIndex = this.logbooks.findIndex(logbook => {
-      return logbook.id == logbookId;
-    })
-    if (logIndex != null) {
-      this.logbooks.splice(logIndex, 1);
-    }
     await this.dataService.deleteLogbook(logbookId);
-    await this.logbookIconScrollService.remove(logbookId)
+    await this.logbookIconScrollService.reload();
     console.log("deleted logbook ", logbookId);
   }
 
@@ -190,8 +141,7 @@ export class OverviewComponent implements OnInit {
     }
     this.subscriptions.push(dialogRef.afterClosed().subscribe(async result => {
       if (typeof result != "undefined") {
-        this.logbooks.push(result);
-        await this.logbookIconScrollService.datasource.adapter.reload()
+        await this.logbookIconScrollService.reload();
       }
     }));
   }
@@ -214,15 +164,6 @@ export class OverviewComponent implements OnInit {
       }
     }
     return _config;
-  }
-
-  intersectLogbooksWithScroller(scrolledData: Logbooks[]): Logbooks[] {
-    if (this.logbooks)
-      return this.logbooks.filter(
-        logbook => scrolledData.some(
-          scrolled => logbook.id === scrolled.id
-        )
-      );
   }
 
   ngOnDestroy(): void {
