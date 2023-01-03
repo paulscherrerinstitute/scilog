fabe8d6e57506381419b76d1cad24a1b36c48b86
minottic <carlo.minotti1@gmail.com>
2023-01-03 15:30:02 +0100

    Fix typeerror lazy loading problem
    More details here: https://stackoverflow.com/questions/74308059/using-typeguard-decorator-typechecked-in-python-whilst-evading-circular-impor

---
+++ sdk/python/tests/test_scilog.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from scilog import SciLog, LogbookMessage
+
+
+@patch.object(SciLog, "post_snippet")
+def test_send_logbook_message(mock_post_snippet):
+    msg = LogbookMessage()
+    msg.add_text("some text")
+    log = SciLog("fake_url")
+    log.send_logbook_message(msg)
+    mock_post_snippet.assert_called_with(
+        linkType="paragraph", textcontent="<p>some text</p>", snippetType="paragraph"
+    )
