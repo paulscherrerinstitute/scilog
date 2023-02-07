42bd8f4f0494419823ce293032982a4a84b79f89
minottic <carlo.minotti1@gmail.com>
2023-02-07 16:52:00 +0100

    Add black profile to avoid conflicts

--- sdk/python/scilog/scilog.py
+++ sdk/python/scilog/scilog.py
@@ -13,8 +13,7 @@ from typeguard import typechecked
 import scilog.logbook_message as lm
 from .authmixin import HEADER_JSON, AuthError
 from .httpclient import HttpClient
-from .snippet import (Basesnippet, Filesnippet, Location, Paragraph,
-                      snippet_factory)
+from .snippet import Basesnippet, Filesnippet, Location, Paragraph, snippet_factory
 
 ACLS = ["createACL", "readACL", "updateACL", "deleteACL", "shareACL", "adminACL"]
 
