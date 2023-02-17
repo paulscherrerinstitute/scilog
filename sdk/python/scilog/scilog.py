b58e2e119e8057e92459e0af6c27ea0b8790da5c
Carlo Minotti <50220438+minottic@users.noreply.github.com>
2023-02-17 10:40:25 +0100

    Add post_snippet method to scilog from scilogcore

--- sdk/python/scilog/scilog.py
+++ sdk/python/scilog/scilog.py
@@ -389,6 +389,10 @@ class SciLog:
             self.http_client.get_request(url, params=params, headers=HEADER_JSON)
         )
 
+    # TODO: review and potentially move the whole core logic here
+    def post_snippet(self, **kwargs):
+        return self.core.post_snippet(**kwargs)
+
 
 class SciLogAuthError(AuthError):
     pass
