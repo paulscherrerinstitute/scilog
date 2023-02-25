ecd24e2eb100d5626584bf59e1b7e2c6dfa42ba1
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-02-25 11:56:30 +0100

    refactor: cleaned up snippet-content and removed deprecated blob handling

--- scilog/src/app/logbook/core/snippet/snippet-content/snippet-content.component.spec.ts
+++ scilog/src/app/logbook/core/snippet/snippet-content/snippet-content.component.spec.ts
@@ -14,9 +14,9 @@ describe('SnippetContentComponent', () => {
       providers: [
         { provide: AppConfigService, useValue: { getConfig } }
       ],
-      declarations: [ SnippetContentComponent ]
+      declarations: [SnippetContentComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -29,9 +29,9 @@ describe('SnippetContentComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should load image', ()=>{
+  it('should load image with fileId', () => {
     spyOn<any>(component, '_srcUrlFromImageId').and.callThrough();
-    let snippetMock =   {
+    let snippetMock = {
       "id": "6061c0283587f37b851694c8",
       "ownerGroup": "p16298",
       "accessGroups": [
@@ -66,14 +66,72 @@ describe('SnippetContentComponent', () => {
     component.snippet = snippetMock;
     component.prepareContent();
 
-    expect((component.figures[0].firstChild as HTMLImageElement).src).toEqual(`http://localhost:3000/images/${snippetMock.files[0].fileId}`)
-    expect((component.figures[0] as HTMLAnchorElement).href).toEqual(`http://localhost:3000/images/${snippetMock.files[0].fileId}`)
+    expect(component.span.children.length).toBe(1);
+
+    const figure = component.span.firstChild;
+    const link = figure.firstChild;
+    const img = link.firstChild;
+
+    expect(link).toBeInstanceOf(HTMLAnchorElement);
+    expect(img).toBeInstanceOf(HTMLImageElement);
+
+    expect(link.href).toEqual(`http://localhost:3000/images/${snippetMock.files[0].fileId}`);
+    expect(img.src).toEqual(`http://localhost:3000/images/${snippetMock.files[0].fileId}`);
     expect(component['_srcUrlFromImageId']).toHaveBeenCalledWith(snippetMock.files[0].fileId);
     expect(component['_srcUrlFromImageId']).toHaveBeenCalledTimes(1);
 
+  })
+
+  it('should load image with accessHash', () => {
+    spyOn<any>(component, '_srcUrlFromImageId').and.callThrough();
+    let snippetMock = {
+      "id": "6061c0283587f37b851694c8",
+      "ownerGroup": "p16298",
+      "accessGroups": [
+        "slscsaxs"
+      ],
+      "snippetType": "paragraph",
+      "isPrivate": false,
+      "defaultOrder": 1617018920128000,
+      "createdAt": "2021-03-29T11:55:20.128Z",
+      "createdBy": "wakonig_k@psi.ch",
+      "updatedAt": "2021-03-29T12:43:20.630Z",
+      "updatedBy": "wakonig_k@psi.ch",
+      "parentId": "602d4338daa91a637da213c4",
+      "tags": [],
+      "versionable": true,
+      "deleted": false,
+      "linkType": "paragraph",
+      "textcontent": "<figure class=\"image image_resized\"><img src=\"\" title=\"60f9799b-e8bb-41c8-a1a7-5f88d35ea851\"></figure>",
+      "files": [
+        {
+          "style": {
+            "width": "87.74%",
+            "height": ""
+          },
+          "fileExtension": "image/png",
+          "fileHash": "60f9799b-e8bb-41c8-a1a7-5f88d35ea851",
+          "fileId": "6061c0283587f37b851694c7",
+          "accessHash": "083f692e8a34f069ea50602167d75f1dd1a43e9f4bf7c03d9ba35cf1390bd22c80f6656eba46abbe5beb5832560050e2a996945e1a07934d0e90e98f6730e006"
+        }
+      ],
+      "id_session": "438"
+    };
+    component.snippet = snippetMock;
     component.prepareContent();
 
-    expect((component.figures[0].firstChild as HTMLImageElement).src).toEqual(`http://localhost:3000/images/${snippetMock.files[0].fileId}`)
+    expect(component.span.children.length).toBe(1);
+
+    const figure = component.span.firstChild;
+    const link = figure.firstChild;
+    const img = link.firstChild;
+
+    expect(link).toBeInstanceOf(HTMLAnchorElement);
+    expect(img).toBeInstanceOf(HTMLImageElement);
+
+    expect(link.href).toEqual(`http://localhost:3000/images/${snippetMock.files[0].accessHash}`);
+    expect(img.src).toEqual(`http://localhost:3000/images/${snippetMock.files[0].accessHash}`);
+    expect(component['_srcUrlFromImageId']).toHaveBeenCalledWith(snippetMock.files[0].accessHash);
     expect(component['_srcUrlFromImageId']).toHaveBeenCalledTimes(1);
 
   })
