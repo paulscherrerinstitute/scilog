2da2dd3a21a4e19b6e9181f4d8b0925d3d0bad11
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-04-22 16:04:01 +0200

    refactor: simplified prismjs import

--- scilog/src/app/logbook/core/prism.service.ts
+++ scilog/src/app/logbook/core/prism.service.ts
@@ -6,20 +6,7 @@ import { isPlatformBrowser } from '@angular/common';
 import 'prismjs';
 import 'prismjs/plugins/toolbar/prism-toolbar';
 // import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-python';
-import 'prismjs/components/prism-julia';
-import 'prismjs/components/prism-matlab';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-c';
-import 'prismjs/components/prism-cpp';
-import 'prismjs/components/prism-markdown';
-import 'prismjs/components/prism-markup';
-import 'prismjs/components/prism-docker';
-import 'prismjs/components/prism-json';
-import 'prismjs/components/prism-yaml';
+import "prismjs-components-importer/esm";
 
 declare var Prism: any;
 
