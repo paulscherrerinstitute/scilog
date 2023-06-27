b30bdb3d8933bf22b6a36ae49ec5446bffcdc236
Stephan Egli <stephan.egli@psi.ch>
2023-06-27 14:08:02 +0200

    Add LICENSE for SDK

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -1,16 +1,5 @@
-from distutils.core import setup
+# setup.py
 
-setup(
-    name="scilog",
-    version="1.0",
-    description="Scilog SDK",
-    author="Klaus Wakonig",
-    author_email="klaus.wakonig@psi.ch",
-    packages=[
-        "scilog",
-    ],
-    install_requires=[
-        "requests >= 2.28.1",
-        "typeguard==2.13.3",
-    ],
-)
+from setuptools import setup
+
+setup()
