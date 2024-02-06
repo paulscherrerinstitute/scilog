182b94cb3bc69929cd895a0583bea60bda438ff4
minottic <carlo.minotti1@gmail.com>
2024-02-06 12:59:48 +0100

    Make group size of overview logbooks dynamic

--- scilog/src/app/overview/logbook-icon-scroll-service.service.spec.ts
+++ scilog/src/app/overview/logbook-icon-scroll-service.service.spec.ts
@@ -2,15 +2,18 @@ import { TestBed } from '@angular/core/testing';
 import { LogbookDataService } from '@shared/remote-data.service';
 
 import { LogbookIconScrollService } from './logbook-icon-scroll-service.service';
+import { AdapterMethodResult, IDatasource } from 'ngx-ui-scroll/src/component/interfaces';
 
 describe('LogbookIconScrollServiceService', () => {
   let service: LogbookIconScrollService;
 
   beforeEach(() => {
-    const LogbookDataServiceSpy = jasmine.createSpyObj("LogbookDataService", ["getDataBuffer"]);
+    const logbookDataServiceSpy = jasmine.createSpyObj("LogbookDataService", ["getDataBuffer"]);
+    logbookDataServiceSpy.getDataBuffer.and.resolveTo([1, 2, 3, 4, 5, 6, 7]);
+
     TestBed.configureTestingModule({
       providers: [
-        {provide: LogbookDataService, useValue: LogbookDataServiceSpy}
+        {provide: LogbookDataService, useValue: logbookDataServiceSpy}
       ],
     });
     service = TestBed.inject(LogbookIconScrollService);
@@ -19,4 +22,10 @@ describe('LogbookIconScrollServiceService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should test getData', async () => {
+    service['datasource'] = {adapter: {relax: async () => ({} as AdapterMethodResult)}} as IDatasource;
+    expect(await service.getData(0, 10, {})).toEqual([[1, 2, 3], [4, 5, 6], [7]]);
+  });
+
 });
