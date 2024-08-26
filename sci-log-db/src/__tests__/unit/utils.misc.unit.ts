88b960a4e051122ef555ca5c4199ef025c670013
minottic <carlo.minotti1@gmail.com>
2024-08-26 16:56:29 +0200

    Fix websocket match filter not filtering

--- sci-log-db/src/__tests__/unit/utils.misc.unit.ts
+++ sci-log-db/src/__tests__/unit/utils.misc.unit.ts
@@ -14,6 +14,7 @@ import {
   sanitizeTextContent,
   sanitizeTextContentInPlace,
 } from '../../utils/misc';
+import {matchesFilterSettings} from '../../utils/websocket';
 
 describe('Utils unit tests', function (this: Suite) {
   it('Should filterEmptySubsnippets', () => {
@@ -314,6 +315,56 @@ describe('Utils unit tests', function (this: Suite) {
     it(`Should test sanitizeTextContentInPlace ${i}`, () => {
       sanitizeTextContentInPlace(t.input);
       expect(t.input).to.be.eql(t.expected);
+    });
+  });
+
+  [
+    {
+      input: [{tags: ['a']}, {filter: {}}],
+      expected: true,
+    },
+    {
+      input: [{tags: ['a', 'p']}, {filter: {tags: ['b', 'c']}}],
+      expected: false,
+    },
+    {
+      input: [{snippetType: 'anotherType'}, {filter: {snippetType: ['aType']}}],
+      expected: false,
+    },
+    {
+      input: [{snippetType: 'aType'}, {filter: {snippetType: ['aType']}}],
+      expected: true,
+    },
+    {
+      input: [{tags: ['a', 'p']}, {filter: {tags: ['a', 'c']}}],
+      expected: true,
+    },
+    {
+      input: [
+        {tags: ['a', 'p'], snippetType: 'anotherType'},
+        {filter: {tags: ['a', 'c'], snippetType: ['aType']}},
+      ],
+      expected: false,
+    },
+    {
+      input: [
+        {tags: ['a', 'p'], snippetType: 'aType'},
+        {filter: {tags: ['a', 'c'], snippetType: ['aType']}},
+      ],
+      expected: true,
+    },
+    {
+      input: [
+        {tags: ['b', 'p'], snippetType: 'aType'},
+        {filter: {tags: ['a', 'c'], snippetType: ['aType']}},
+      ],
+      expected: false,
+    },
+  ].forEach((t, i) => {
+    it(`Should test matchesFilterSettings ${i}`, () => {
+      expect(
+        matchesFilterSettings(t.input[0] as Basesnippet, t.input[1]),
+      ).to.be.eql(t.expected);
     });
   });
 });
