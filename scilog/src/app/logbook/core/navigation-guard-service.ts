6cbae495b5528efd2de067d5468d6042d7982818
Omkar Zade <omkar.zade@psi.ch>
2025-05-06 14:12:05 +0200

    Apply angular and cli upgrade to v16 -- ng update changes only

--- scilog/src/app/logbook/core/navigation-guard-service.ts
+++ scilog/src/app/logbook/core/navigation-guard-service.ts
@@ -1,4 +1,4 @@
-import { CanDeactivate } from '@angular/router';
+
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,7 +7,7 @@ export interface ComponentCanDeactivate {
 }
 
 @Injectable()
-export class NavigationGuardService implements CanDeactivate<ComponentCanDeactivate> {
+export class NavigationGuardService  {
 
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
     return component.canDeactivate() || confirm('You are about to leave the page. Do you want to continue?');
