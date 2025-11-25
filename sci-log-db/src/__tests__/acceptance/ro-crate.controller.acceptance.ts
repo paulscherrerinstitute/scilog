f09c6529482e4860d77f9a98ceba75f57d0d68fc
Omkar Zade <omkar.zade@psi.ch>
2025-11-25 16:08:48 +0100

    Add test for zip download endpoint

--- sci-log-db/src/__tests__/acceptance/ro-crate.controller.acceptance.ts
+++ sci-log-db/src/__tests__/acceptance/ro-crate.controller.acceptance.ts
@@ -3,8 +3,12 @@ import {Suite} from 'mocha';
 import {SciLogDbApplication} from '../..';
 import {clearDatabase, createUserToken, setupApplication} from './test-helper';
 import {DatabaseHelper} from '../database.helpers';
+import fs from 'fs';
+import yauzl from 'yauzl';
 // @ts-ignore
 import {ROCrate} from 'ro-crate';
+import {pipeline, Readable} from 'stream';
+import { RoCrateController } from '../../controllers';
 
 describe('RocrateController', function (this: Suite) {
   this.timeout(5000);
@@ -14,6 +18,7 @@ describe('RocrateController', function (this: Suite) {
   let databaseHelper: DatabaseHelper;
 
   const LOGBOOK_OWNER_GROUP = 'roCrateAcceptance';
+  const ZIP_FILE_PATH = 'roCrateAcceptanceTest.zip';
 
   const user = {
     email: 'test@loopback.io',
@@ -56,6 +61,8 @@ describe('RocrateController', function (this: Suite) {
         // sanity checks that it's a valid ro-crate
         const crate = new ROCrate(result.body);
         expect(crate.rootDataset.name).to.equal(logbookSnippet.name);
+
+        // check properties of logbook entity
         const logbookEntity = crate.getEntity(`./${logbook.id}/`);
         expect(crate.hasType(logbookEntity, 'Book')).to.be.true();
         expect(logbookEntity.description).to.equal(logbookSnippet.description);
@@ -69,4 +76,55 @@ describe('RocrateController', function (this: Suite) {
         throw error;
       });
   });
+
+  it('downloads rocrate archive for existing logbook', async () => {
+    const logbook = await databaseHelper.givenLogbook(logbookSnippet, {
+      currentUser: user,
+    });
+    await client
+      .get(`/rocrates/${logbook.id}/download`)
+      .set('Authorization', 'Bearer ' + token)
+      .responseType('blob')
+      .expect(200)
+      .then(async response => {
+        const outputStream = fs.createWriteStream(ZIP_FILE_PATH);
+        const inputStream = Readable.from(response.body);
+        return new Promise<void>((resolve, reject) => {
+          pipeline(inputStream, outputStream, err => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        });
+      })
+      .then(async () => {
+        const files = await listZipEntries(ZIP_FILE_PATH);
+        expect(files).to.containEql(`${RoCrateController.ARCHIVE_ROOT}/ro-crate-metadata.json`);
+        expect(files).to.containEql(`${RoCrateController.ARCHIVE_ROOT}/ro-crate-preview.html`);
+        fs.unlinkSync(ZIP_FILE_PATH);
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
 });
+
+function listZipEntries(zipPath: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    const entries: string[] = [];
+
+    yauzl.open(zipPath, {lazyEntries: true}, (err, zipfile) => {
+      if (err) return reject(err);
+
+      zipfile.readEntry();
+      zipfile.on('entry', entry => {
+        entries.push(entry.fileName);
+        zipfile.readEntry();
+      });
+
+      zipfile.on('end', () => resolve(entries));
+    });
+  });
+}
