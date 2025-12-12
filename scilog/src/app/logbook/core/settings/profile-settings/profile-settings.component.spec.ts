aa7638d9b729ee779445d5bfa2b85c6517b261fa
Omkar Zade <omkar.zade@psi.ch>
2025-12-12 14:55:33 +0100

    add scilog-token and copy-to-clipboard functionality in user-profile dialog (#460)

--- scilog/src/app/logbook/core/settings/profile-settings/profile-settings.component.spec.ts
+++ scilog/src/app/logbook/core/settings/profile-settings/profile-settings.component.spec.ts
@@ -4,6 +4,8 @@ import { ProfileSettingsComponent } from './profile-settings.component';
 import { UserPreferencesService } from '@shared/user-preferences.service';
 import { UntypedFormBuilder } from '@angular/forms';
 import { AppConfigService } from 'src/app/app-config.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '@shared/auth-services/auth.service';
 
 
 class UserPreferencesMock {
@@ -11,6 +13,16 @@ class UserPreferencesMock {
     roles: ["roles"]
 
   }
+}
+
+class AuthServiceMock {
+  getScilogToken() {
+    return 'mock-token-123';
+  }
+}
+
+class MatSnackBarMock {
+  open() {}
 }
 
 const getConfig = () => ({});
@@ -25,7 +37,9 @@ describe('ProfileSettingsComponent', () => {
     providers: [
         UntypedFormBuilder,
         { provide: UserPreferencesService, useClass: UserPreferencesMock },
-        { provide: AppConfigService, useValue: { getConfig } }
+        { provide: AppConfigService, useValue: { getConfig } },
+        { provide: MatSnackBar, useClass: MatSnackBarMock },
+        { provide: AuthService, useClass: AuthServiceMock }
     ]
 })
     .compileComponents();
@@ -39,5 +53,11 @@ describe('ProfileSettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('copies token to clipboard', () => {
+    const clipboardSpy = spyOn(navigator.clipboard, 'writeText').and.resolveTo(void 0);
+    component.copyToClipboard();
+    expect(clipboardSpy).toHaveBeenCalledWith('mock-token-123');
   });
 });
--- scilog/src/app/logbook/core/settings/profile-settings/profile-settings.component.ts
+++ scilog/src/app/logbook/core/settings/profile-settings/profile-settings.component.ts
@@ -1,39 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { UserPreferencesService } from '@shared/user-preferences.service';
 import { UntypedFormBuilder, UntypedFormGroup, UntypedFormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '@shared/auth-services/auth.service';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-    selector: 'app-profile-settings',
-    templateUrl: './profile-settings.component.html',
-    styleUrls: ['./profile-settings.component.css'],
-    imports: [FormsModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput]
+  selector: 'app-profile-settings',
+  templateUrl: './profile-settings.component.html',
+  styleUrls: ['./profile-settings.component.css'],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class ProfileSettingsComponent implements OnInit {
-
   formBuilder: UntypedFormBuilder;
   profileFormGroup: UntypedFormGroup;
 
-
   constructor(
     private userPreferences: UserPreferencesService,
-    fb: UntypedFormBuilder,
-  ) { 
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    fb: UntypedFormBuilder
+  ) {
     this.formBuilder = fb;
-
   }
 
   ngOnInit(): void {
     this.profileFormGroup = this.formBuilder.group({
-      name: new UntypedFormControl({ value: this.userPreferences.userInfo.firstName, disabled: true }),
-      lastname: new UntypedFormControl({ value: this.userPreferences.userInfo.lastName, disabled: true }),
-      email: new UntypedFormControl({ value: this.userPreferences.userInfo.email, disabled: true }),
-      username: new UntypedFormControl({ value: this.userPreferences.userInfo.email, disabled: true }),
-
+      name: new UntypedFormControl({
+        value: this.userPreferences.userInfo.firstName,
+        disabled: true,
+      }),
+      lastname: new UntypedFormControl({
+        value: this.userPreferences.userInfo.lastName,
+        disabled: true,
+      }),
+      email: new UntypedFormControl({
+        value: this.userPreferences.userInfo.email,
+        disabled: true,
+      }),
+      username: new UntypedFormControl({
+        value: this.userPreferences.userInfo.email,
+        disabled: true,
+      }),
+      token: new UntypedFormControl({
+        value: this.authService.getScilogToken(),
+        disabled: true,
+      }),
     });
-    console.log(this.userPreferences.userInfo)
-    
   }
 
+  copyToClipboard() {
+    const token = this.profileFormGroup.get('token')?.value;
+    if (token) {
+      navigator.clipboard
+        .writeText(token)
+        .then(() => {
+          this.snackBar.open('Token copied to clipboard', 'Dismiss', {
+            duration: 3000,
+            verticalPosition: 'top',
+          });
+        })
+        .catch((err) => {
+          console.error('Failed to copy token: ', err);
+          this.snackBar.open('Failed to copy token to clipboard', 'Dismiss', {
+            duration: 3000,
+            verticalPosition: 'top',
+          });
+        });
+    }
+  }
 }
