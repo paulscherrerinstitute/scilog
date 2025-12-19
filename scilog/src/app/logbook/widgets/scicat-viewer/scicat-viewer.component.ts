01a056e971d4be89460e7b5dee245a3f4e7b497e
Omkar Zade <omkar.zade@psi.ch>
2025-12-19 12:42:42 +0100

    feat: link on proposalId in scicat-viewer widget

--- scilog/src/app/logbook/widgets/scicat-viewer/scicat-viewer.component.ts
+++ scilog/src/app/logbook/widgets/scicat-viewer/scicat-viewer.component.ts
@@ -2,23 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { MatSelectChange, MatSelect, MatOption } from '@angular/material/select';
 import {
   type Dataset,
-  DatasetService,
+  ScicatService,
   type ScicatUser,
   type DatasetSummary,
-} from '@shared/dataset.service';
+} from '@shared/scicat.service';
 import { NgIf, NgFor, DatePipe } from '@angular/common';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
-import {
-  MatCard,
-  MatCardHeader,
-  MatCardTitle,
-  MatCardSubtitle,
-  MatCardContent,
-  MatCardActions,
-} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatAnchor } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { LogbookInfoService } from '@shared/logbook-info.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-scicat-viewer',
@@ -31,12 +26,7 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
     MatSelect,
     NgFor,
     MatOption,
-    MatCard,
-    MatCardHeader,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatCardContent,
-    MatCardActions,
+    MatCardModule,
     MatAnchor,
     MatIcon,
     MatProgressSpinner,
@@ -44,15 +34,19 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
   ],
 })
 export class ScicatViewerComponent implements OnInit {
-  constructor(private datasetService: DatasetService) {}
+  constructor(
+    private scicatService: ScicatService,
+    private logbookInfoService: LogbookInfoService,
+  ) {}
 
   scicatUser: ScicatUser | null = null;
   datasetSummary: DatasetSummary[] = [];
   selectedDataset: Dataset | null = null;
   datasetFetching = false;
+  proposalLinkedDatasets: DatasetSummary[] = [];
 
   ngOnInit(): void {
-    this.datasetService.getMyself().subscribe({
+    this.scicatService.getMyself().subscribe({
       next: (data: ScicatUser) => {
         this.scicatUser = data;
       },
@@ -61,20 +55,49 @@ export class ScicatViewerComponent implements OnInit {
       },
     });
 
-    this.datasetService.getDatasets().subscribe((data: DatasetSummary[]) => {
-      this.datasetSummary = data;
-    });
+    const ownerGroup = this.logbookInfoService.logbookInfo.ownerGroup;
+
+    this.scicatService
+      .getDatasets()
+      .pipe(
+        switchMap((datasets: DatasetSummary[]) => {
+          this.datasetSummary = datasets;
+          return this.scicatService.getProposalLinkedDatasets(ownerGroup);
+        }),
+      )
+      .subscribe({
+        next: (proposalDatasets: DatasetSummary[]) => {
+          if (!proposalDatasets?.length) return;
+          this.proposalLinkedDatasets = proposalDatasets;
+          // add new proposal linked datasets to selection, if not already present
+          const datasetsToAdd = proposalDatasets.filter(
+            (ds) => !this.datasetSummary.some((d) => d.pid === ds.pid),
+          );
+          this.datasetSummary = [...datasetsToAdd, ...this.datasetSummary];
+          // select the first proposal-linked dataset by default
+          this.onDatasetSelect({ value: proposalDatasets[0].pid } as MatSelectChange);
+        },
+        error: (err) => console.log('Error in getDatasets or getproposalLinkedDatasets', err),
+      });
+  }
+
+  isProposalLinkedDataset(pid: string): boolean {
+    return this.proposalLinkedDatasets.some((dataset) => dataset.pid === pid);
   }
 
   get scicatDatasetUrl(): string {
-    return this.datasetService.getDatasetDetailPageUrl(this.selectedDataset?.pid);
+    return this.scicatService.getDatasetDetailPageUrl(this.selectedDataset?.pid);
+  }
+
+  get scicatProposalUrl(): string {
+    return this.scicatService.getProposalPageUrl(this.selectedDataset?.proposalId);
   }
 
   onDatasetSelect(event: MatSelectChange): void {
     const selectedPid = event.value;
     this.datasetFetching = true;
     this.selectedDataset = null;
-    this.datasetService.getDataset(selectedPid).subscribe((data: Dataset) => {
+    this.scicatService.getDataset(selectedPid).subscribe((data: Dataset) => {
       this.selectedDataset = data;
       this.datasetFetching = false;
     });
