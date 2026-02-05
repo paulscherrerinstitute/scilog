8c5eb0fc129df32930886bd285110c8c87d15526
wakonig_k <klaus.wakonig@psi.ch>
2026-02-05 15:49:51 +0100

    build(python): upgrade typeguard dependency

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,15 +2,10 @@ from distutils.core import setup
 
 setup(
     name="scilog",
-    version="1.0",
+    version="1.1",
     description="Scilog SDK",
-    author="Klaus Wakonig",
-    author_email="klaus.wakonig@psi.ch",
-    packages=[
-        "scilog",
-    ],
-    install_requires=[
-        "requests >= 2.28.1",
-        "typeguard==2.13.3",
-    ],
+    author="Paul Scherrer Institute, Switzerland",
+    author_email="scilog-help@lists.psi.ch",
+    packages=["scilog"],
+    install_requires=["requests >= 2.28.1", "typeguard~=4.0"],
 )
