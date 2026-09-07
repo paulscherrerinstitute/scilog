a04c5901b930f91e4725768ed1e7ce460c35e519
Omkar Zade <omkar.zade@psi.ch>
2026-09-07 14:47:57 +0200

    feat: add endpoint to export logbook as a dataset packaged as scicat-rocrate

--- api/src/services/archive.service.ts
+++ api/src/services/archive.service.ts
@@ -3,7 +3,12 @@ import archiver from 'archiver';
 import {finished, Readable} from 'node:stream';
 
 export interface AssetDescriptor {
-  stream: Readable;
+  // A null stream + an archivePath with trailing slash indicates
+  // to zipStream to create a directory entry
+  // This is NOT required by the zip format, but only a temporary
+  // measure until the following
+  // TO-DO: Remove once https://github.com/paulscherrerinstitute/scicat-rocrate/issues/338 deployed
+  stream: Readable | null;
   archivePath: string;
 }
 
@@ -13,11 +18,18 @@ export class ArchiveService {
   zipStream(assets: AssetDescriptor[]): Readable {
     const archive = archiver('zip');
     for (const {stream, archivePath} of assets) {
+      // TO-DO: Remove this branch
+      // once https://github.com/paulscherrerinstitute/scicat-rocrate/issues/338 deployed
+      if (stream === null) {
+        // archiver reads the trailing slash and writes a directory entry.
+        archive.append('', {name: archivePath});
+        continue;
+      }
       stream.on('error', err => archive.destroy(err));
       archive.append(stream, {name: archivePath});
     }
 
-    finished(archive, () => assets.forEach(({stream}) => stream.destroy()));
+    finished(archive, () => assets.forEach(({stream}) => stream?.destroy()));
 
     archive.finalize().catch(err => archive.destroy(err));
     return archive;
