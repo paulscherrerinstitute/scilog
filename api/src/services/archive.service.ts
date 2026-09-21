fce5b91c1d6c59e5dc8da360ddd310e5cccfd0ac
Omkar Zade <omkar.zade@psi.ch>
2026-09-21 11:29:45 +0000

    chore(deps): bump archiver to 8.0.0 (#750)
    In preparation for
    https://github.com/paulscherrerinstitute/scilog/pull/741 , updating the
    archiver dependency and it's callsites with the new signature. Will
    attempt to make this PR the bottommost of the stack

--- api/src/services/archive.service.ts
+++ api/src/services/archive.service.ts
@@ -1,7 +1,7 @@
-import {injectable, BindingScope} from '@loopback/core';
-import archiver from 'archiver';
+import {BindingScope, injectable} from '@loopback/core';
 import {Response} from '@loopback/rest';
-import {pipeline, Readable} from 'stream';
+import {ZipArchive} from 'archiver';
+import {pipeline, Readable} from 'node:stream';
 
 export interface AssetDescriptor {
   stream: Readable;
@@ -17,7 +17,7 @@ export class ArchiveService {
     res: Response,
   ): Promise<void> {
     return new Promise((resolve, reject) => {
-      const archive = archiver('zip');
+      const archive = new ZipArchive();
 
       for (const asset of assets) {
         archive.append(asset.stream, {name: asset.archivePath});
